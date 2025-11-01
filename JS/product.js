9ad3fe8b5c63ac4044537ad327cbfc1d0ad7b1ac
Conrad-prog-cpu <lr.csantos@mmdc.mcl.edu.ph>
2025-11-01 09:17:55 +0800

    adding product.js to modularize codes

---
+++ JS/product.js
@@ -0,0 +1,52 @@
+ // Menu Data
+  export const menuData = [
+    {
+      section: "Hot Coffee",
+      items: [
+        { name: "Coffee Americano", img: "../PICTURES/MENU/HOT_COFFEE.png", desc: "Rich espresso served hot.", sizes: { "Small": 60, "Medium": 70, "Large": 85 } },
+        { name: "Coffee Latte", img: "../PICTURES/MENU/HOT_COFFEE_LATTE_01.png", desc: "Smooth espresso with steamed milk.", sizes: { "Small": 70, "Medium": 80, "Large": 95 } },
+        { name: "Coffee Matcha", img: "../PICTURES/MENU/HOT_COFFEE_MATCHA.png", desc: "Stone-ground matcha with milk.", sizes: { "Small": 75, "Medium": 85, "Large": 100 } },
+        { name: "Hot Choco", img: "../PICTURES/MENU/HOT_CHOCO.png", desc: "Chocolate goodness with steamed milk.", sizes: { "Small": 75, "Medium": 85, "Large": 100 } },
+        { name: "Hot Tea", img: "../PICTURES/MENU/HOT_TEA.png", desc: "Aromatic brewed tea.", sizes: { "Small": 60, "Medium": 70, "Large": 85 } }
+      ]
+    },
+    {
+      section: "Iced Cold Coffee",
+      items: [
+        { name: "Iced Americano", img: "../PICTURES/MENU/COLD COFFEE_00.png", desc: "Chilled espresso.", sizes: { "Small": 70, "Medium": 80, "Large": 90 } },
+        { name: "Iced Latte", img: "../PICTURES/MENU/COLD COFFEE_01.png", desc: "Espresso with cold milk.", sizes: { "Small": 70, "Medium": 80, "Large": 95 } },
+        { name: "Iced Latte Milk", img: "../PICTURES/MENU/COLD COFFEE_02.png", desc: "Creamy iced latte.", sizes: { "Small": 75, "Medium": 85, "Large": 100 } },
+        { name: "Iced Matcha", img: "../PICTURES/MENU/COLD COFFEE_03.png", desc: "Refreshing matcha iced drink.", sizes: { "Small": 75, "Medium": 85, "Large": 100 } },
+        { name: "Iced Milk", img: "../PICTURES/MENU/COLD COFFEE_04.png", desc: "Cold milk beverage.", sizes: { "Small": 60, "Medium": 70, "Large": 85 } }
+      ]
+    },
+    {
+      section: "Iced Cold Drink with Fraff",
+      items: [
+        { name: "Coffee Fraff", img: "../PICTURES/MENU/FRAFF_01.png", desc: "Blended coffee with ice cream.", sizes: { "Small": 75, "Medium": 85, "Large": 100 } },
+        { name: "Choco Fraff", img: "../PICTURES/MENU/FRAFF_02.png", desc: "Chocolate blended drink.", sizes: { "Small": 75, "Medium": 85, "Large": 100 } },
+        { name: "Taho Fraff", img: "../PICTURES/MENU/FRAFF_03.png", desc: "Tofu blended dessert drink.", sizes: { "Small": 70, "Medium": 80, "Large": 95 } },
+        { name: "Vanilla Fraff", img: "../PICTURES/MENU/FRAFF_04.png", desc: "Vanilla ice blended drink.", sizes: { "Small": 75, "Medium": 85, "Large": 100 } },
+        { name: "Ube Vanilla Fraff", img: "../PICTURES/MENU/FRAFF_05.png", desc: "Ube and vanilla blended.", sizes: { "Small": 80, "Medium": 90, "Large": 105 } },
+        { name: "Buko Salad Fraff", img: "../PICTURES/MENU/FRAFF_06.png", desc: "Coconut salad blended.", sizes: { "Small": 80, "Medium": 90, "Large": 105 } }
+      ]
+    },
+    {
+      section: "Juice",
+      items: [
+        { name: "Strawberry Juice", img: "../PICTURES/MENU/JUICE_01.png", desc: "Fresh strawberry juice.", sizes: { "Small": 70, "Medium": 80, "Large": 90 } },
+        { name: "Sweet Juice", img: "../PICTURES/MENU/JUICE_02.png", desc: "Mixed fruit juice.", sizes: { "Small": 70, "Medium": 80, "Large": 90 } },
+        { name: "Iced Tea", img: "../PICTURES/MENU/JUICE_03.png", desc: "Refreshing iced tea.", sizes: { "Small": 60, "Medium": 70, "Large": 80 } }
+      ]
+    },
+    {
+      section: "Pastries",
+      items: [
+        { name: "Croissant", img: "../PICTURES/MENU/PASTRY_01.png", desc: "Flaky croissant.", sizes: { "Regular": 80 } },
+        { name: "Hopia", img: "../PICTURES/MENU/PASTRY_02.png", desc: "Delicious hopia.", sizes: { "Regular": 85 } },
+        { name: "Donutso", img: "../PICTURES/MENU/PASTRY_03.png", desc: "Sweet donut pastry.", sizes: { "Regular": 85 } },
+        { name: "Coffee Bread", img: "../PICTURES/MENU/PASTRY_04.png", desc: "Coffee-flavored bread.", sizes: { "Regular": 100 } },
+        { name: "Masarap Ito", img: "../PICTURES/MENU/PASTRY_05.png", desc: "Specialty pastry.", sizes: { "Regular": 180 } }
+      ]
+    }
+  ];
